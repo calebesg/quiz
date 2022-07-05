cc12437f199b21cf9733e7c56546a30a1f2815aa
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-05 14:34:40 -0300

    animate flip answer

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -13,37 +13,37 @@ interface AnswerProps {
 export default function Answer(props: AnswerProps) {
   const { background, letter, value } = props;
 
+  const reveled = value.getReveled ? styles.reveled : "";
+
   return (
     <div
       className={styles.answer}
       onClick={() => props.selectQuestion(props.index)}
     >
-      <div className={styles.wrapper}>
-        {!props.value.getReveled ? (
-          <div className={styles.content}>
-            <span
-              style={{ backgroundColor: background }}
-              className={styles.letter}
-            >
-              {letter}
-            </span>
-            <strong className={styles.value}>{value.getValue}</strong>
-          </div>
-        ) : (
-          <div className={styles.sided}>
-            {value.getIsRight ? (
-              <div className={styles.right}>
-                <div>A resposta certa é...</div>
-                <div className={styles.value}>{value.getValue}</div>
-              </div>
-            ) : (
-              <div className={styles.wrong}>
-                <div>A resposta informada está errada...</div>
-                <div className={styles.value}>{value.getValue}</div>
-              </div>
-            )}
-          </div>
-        )}
+      <div className={`${styles.wrapper} ${reveled}`}>
+        <div className={styles.content}>
+          <span
+            style={{ backgroundColor: background }}
+            className={styles.letter}
+          >
+            {letter}
+          </span>
+          <strong className={styles.value}>{value.getValue}</strong>
+        </div>
+
+        <div className={styles.sided}>
+          {value.getIsRight ? (
+            <div className={styles.right}>
+              <div>A resposta certa é...</div>
+              <div className={styles.value}>{value.getValue}</div>
+            </div>
+          ) : (
+            <div className={styles.wrong}>
+              <div>A resposta informada está errada...</div>
+              <div className={styles.value}>{value.getValue}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
