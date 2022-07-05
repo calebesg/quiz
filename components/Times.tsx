9f20faf5b05b76803a810d5fef34fffaefc59d66
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-05 15:01:21 -0300

    fix: remove children props in Timer

--- components/Times.tsx
+++ components/Times.tsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import styles from "../styles/components/Timer.module.css";
 
@@ -7,20 +8,24 @@ interface TimerProps {
   timeOut: () => void;
 }
 
-export function Timer({ duration, timeOut }: TimerProps) {
+export function Timer({ duration, timeOut, key }: TimerProps) {
+  const renderTime = (time: number, color: string) => {
+    return <span style={{ color: color }}>{time}</span>;
+  };
+
   return (
     <div className={styles.timer}>
       <CountdownCircleTimer
+        key={key}
         duration={duration}
         size={120}
         colors={["#BCE596", "#F7B801", "#ED827A"]}
         colorsTime={[10, 5, 0]}
         isPlaying
         onComplete={timeOut}
-        children={({ remainingTime, color }) => (
-          <span style={{ color: color }}>{remainingTime}</span>
-        )}
-      />
+      >
+        {(data) => renderTime(data.remainingTime, data.color)}
+      </CountdownCircleTimer>
     </div>
   );
 }
